8f89f0d19b68f7c58c0659d757d7a2d9414b7c63
ymc <781011762@qq.com>
2017-09-30 09:36:28 +0800

    报修功能 和 登录注册页面

---
+++ application/admin/validate/Repair.php
@@ -0,0 +1,15 @@
+<?php
+ 
+namespace app\admin\validate;
+use think\Validate; 
+
+class Repair extends Validate{
+     
+    protected $rule = [ 
+        ['name', 'require', '报修人不能为空'],
+        ['tel', 'require', '手机号码不能为空'],
+        ['address', 'require', '地址不能为空'],
+        ['title', 'require', '标题不能为空'],
+        ['content', 'length:0,255'],
+    ];
+}
